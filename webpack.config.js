a31d6e7ad5c987606eaaa4f72da31151e3deb89f
ian <ian@smallwalrus.com>
2021-05-13 20:17:12 +0100

    upgrades

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,6 @@ module.exports = {
     hot: true
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
